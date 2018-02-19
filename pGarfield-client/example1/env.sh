852baac666f8f88645276500221d228867e334af
Ali Sheharyar <ali.sheharyar@gmail.com>
2018-02-19 13:58:52 +0300

    Restructed the source code. Placed simulator and client under the same repository

---
+++ pGarfield-client/example1/env.sh
@@ -0,0 +1,5 @@
+module rm PrgEnv-cray/5.2.82
+module load PrgEnv-gnu
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lustre/sw/xc40ac/boost/1.58.0/cnl5.2_gnu5.2.0/lib
+export GARFIELD_HOME=/lustre/sw/xc40ac/garfield/cnl5.2_gnu5.2.0/parallel
+source /lustre/sw/xc40ac/root/v5.34.36/cnl5.2_gnu5.2.0/bin/thisroot.sh
